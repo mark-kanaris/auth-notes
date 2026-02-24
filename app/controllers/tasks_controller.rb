6ae12b03a29e258d826facc2a44ab4ea7eac7d12
mark-kanaris <mark.kanaris@kellogg.northwestern.edu>
2026-02-24 17:28:10 +0000

    tasks stuff - still in progress

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,14 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
+    #@tasks_user = Task.where({@tasks["user_id"] => session["user_id"]})
+
   end
 
   def create
     @task = Task.new
     @task["description"] = params["description"]
+    @tasks["user_id"] = session["user_id"]
     @task.save
     redirect_to "/tasks"
   end
